0f5003bd08dcc2a2b296bb29bde90839c4c6cd98
gabrieledecarvalho <carvalhodegabriele@gmail.com>
2024-09-30 12:33:24 -0300

    modificando o main.py e criando um novo arquivo para o ex 2 chamado main2.py

--- 20240930/main.py
+++ 20240930/main.py
@@ -1,5 +1,5 @@
 #coding utf-8
-import math
+import math as m #criando um apelido para a biblioteca
 # revisão entrada e saída de dados
 
 
@@ -11,11 +11,25 @@ a = 1 # pode colocar o espaço ou não
 b=4
 c=3
 
-delta = b** - 4*a*c # criando uma variável delta
-x1=(-b+delta*0.5)/2*a # resolvendo o x1
-x2=(-b-delta*0.5)/2*a # resolvendo o x2
+#método mais otimizado:
+delta = m.pow(b,2)-4*a*c
+x1=(-b+m.sqrt(delta))/2*a
+x2=(-b-m.sqrt(delta))/2*a
 
-# x2=(-b-math.sqrt(delta))/2*a 
+'''
+delta = b**2-4*a*c # criando uma variável delta
+x1=(-b+delta*0.5)/2*a # resolvendo o x1 de forma lenta pois não usa funções
+x2=(-b-delta*0.5)/2*a # resolvendo o x2
+'''
+
+''' 
+x2=(-b-math.sqrt(delta))/2*a 
+como criei um apelido para a biblioteca math então a linha acima fica como:
+x2=(-b-m.sqrt(delta))/2*a 
+
+a forma mais otimizada para fazer essa equação é utilizar o pow:
+delta = m.pow(b,2)-4*a*c #boa prática de programação
+'''
 
 print("O valor de x1 é: ",x1)
 print("O valor de x2 é: ",x2)
---
+++ 20240930/main2.py
@@ -0,0 +1,9 @@
+#coding utf-8
+
+nome = 'gab'
+altura = 1.67
+numeroSapato = 37
+
+print(type(nome))
+print(type(altura))
+print(type(numeroSapato))
