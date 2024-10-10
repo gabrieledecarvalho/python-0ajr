05532ce9eb5c4adee0bb39c049f1e9da98102310
gabrieledecarvalho <carvalhodegabriele@gmail.com>
2024-10-09 21:23:09 -0300

    Adicionando soluções dos problemas do beecrowd

---
+++ beecrowd/1006-problema.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# média 2
+A = float(input())
+B = float(input())
+C = float(input())
+peso1 = 2
+peso2 = 3
+peso3 = 5
+MEDIA = (A*peso1 + B*peso2 + C*peso3)/(peso1+peso2+peso3)
+print("MEDIA = {0:.1f}".format(MEDIA))
