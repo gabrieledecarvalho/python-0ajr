19e3996b9328283c06e56cfd3ad1698595377c74
gabrieledecarvalho <carvalhodegabriele@gmail.com>
2024-10-14 11:35:38 -0300

    adicionando uma solução para o 1546 do beecrowd

--- beecrowd/dúvida-1546-problema.py
+++ beecrowd/dúvida-1546-problema.py
@@ -1,15 +1,17 @@
 #coding: utf-8
-n = int(input()) #numero de casos de teste
-k = int(input()) #numero de feedbacks do dia
+# programa que classifica feedbacks
+n = int(input()) #numero de casos de teste 1 a 100
+k = int(input()) #numero de feedbacks do dia 1 a 50
 
-i=0
-while(i<k+1):
-    if(n==1):
-        print("Rolien")
-    if(n==2):
-        print("Naej")
-    if(n==3):
-        print("Elehcim")
-    if(n==4):
-        print("Odranoel")
-    i+=1
+for i in range(i<n):
+    for j in range (j<k):
+        categoria = int(input())
+        if(n==1):
+            print("Rolien")
+        if(n==2):
+            print("Naej")
+        if(n==3):
+            print("Elehcim")
+        if(n==4):
+            print("Odranoel")
+        i+=1
