54e2770fdb0f4c1c0d2967d5423570d9e6b675e6
gabrieledecarvalho <carvalhodegabriele@gmail.com>
2024-10-14 11:49:38 -0300

    melhorando o problema 1546 do beecrowd

--- beecrowd/dúvida-1546-problema.py
+++ beecrowd/dúvida-1546-problema.py
@@ -3,15 +3,14 @@
 n = int(input()) #numero de casos de teste 1 a 100
 k = int(input()) #numero de feedbacks do dia 1 a 50
 
-for i in range(i<n):
-    for j in range (j<k):
+for i in range(n):
+    for j in range (k):
         categoria = int(input())
-        if(n==1):
+        if(categoria==1):
             print("Rolien")
-        if(n==2):
+        if(categoria==2):
             print("Naej")
-        if(n==3):
+        if(categoria==3):
             print("Elehcim")
-        if(n==4):
+        if(categoria==4):
             print("Odranoel")
-        i+=1
