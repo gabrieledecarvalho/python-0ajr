ed37c924a6e1a6e12d6fd85e9e452d49e71dad2d
gabrieledecarvalho <carvalhodegabriele@gmail.com>
2024-10-16 13:05:18 -0300

    final da aula 16 oct

--- 20241016/MetodosVetores.py
+++ 20241016/MetodosVetores.py
@@ -1,13 +1,34 @@
 #coding: utf-8
-def Insercao_dado():
-    print("Vetor Original: ", vC)
-    input("Digite enter para prosseguir: ")
+
+def insercaoDadoVetor():
     vC = [1, 3.4, 'A', " IFSC "]
+    print("Vetor Original: ", vC)
+    #input("Digite enter para prosseguir: ")
     vC.insert(0,56) # adiciona na posição 0 o inteiro 56
-    vC.insert(3, 'B')
-    vC.append(11)
-    for i in vC:
+    print("Adicionando o inteiro 56 na posição 8: ", vC)
+    #input("Digite enter para prosseguir: ")
+    vC.insert(3, 'B') # adiciona na posição 3 o caracter B
+    print("Adicionando o caracter B na posição 3: ", vC)
+    #input("Digite enter para prosseguir: ")
+    vC.append(11) # adiciona no final do vetor, independente do número de posição, o inteiro 11
+    print("Forma do vetor final em coluna: ", vC)
+    #input("Digite enter para prosseguir: ")
+    for i in vC: # printa o vetor vC em forma de coluna
         print(i)
+    print("Fim da função inserção de dados no vetor!")
+    return vC
+
+def remocaoDadoVetor(vet):
+    print("Vetor recebido: ", vet)
+    print("Elemento 'A' removido")
+    vet.remove('A')
+    print("Vetor alterado: ", vet)
+    input("Digite enter para prosseguir: ")
+
+    print("Fim da função que remove elemento")
 
 if __name__ == "__main__":
-    Insercao_dado()
+    vet = Insercao_dado()
+    remocaoDadoVetor(vet)
+    
+    print("Fim do programa!") # monsta a mensagem no final do programa que o fim do programa chegou
--- 20241016/main.py
+++ 20241016/main.py
@@ -1,9 +1,9 @@
 #coding: utf-8
-import FuncoesEntrada as FE # importando as funções escritas no programa anterior como se fosse uma biblioteca
+import FuncoesEntradas as FE # importando as funções escritas no programa anterior como se fosse uma biblioteca
 import MetodosVetores as MV
 
 if __name__ == "__main__":
-    MV.Insercao_dado()
+    MV.insercaoDadoVetor()
     #FE.forma1Leitura()
     #FE.forma2Leitura()
     #FE.forma1LeituraArray()
