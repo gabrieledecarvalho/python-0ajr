08baaee3abc0fc0cc068fbb4776b83bd6a54e248
gabrieledecarvalho <carvalhodegabriele@gmail.com>
2024-10-16 11:44:40 -0300

    atualizando alguns programas

--- 20241014/exercicio.py
+++ 20241014/exercicio.py
@@ -1,4 +1,17 @@
 #coding: utf-8
-entrada = input().split
+# não sei qual é esse exercício do beecrowd
+# Crie um programa que leia uma linha contendo vários números inteiros
+# separados por espaço, armazene em um vetor e altere cada elemento
+# multiplicando o mesmo por 3. Por fim, imprima cada elemento multiplicado
+# por 3 em uma linha e separados por espaço
+
+
+
+entrada = input()
+vetor = []
+vetor = entrada.split(" ")
+
 for i in range(len(entrada)):
-    entrada[i] = entrada[i]*3
+    vetor[i] = vetor[i]*3
+
+print(vetor)
